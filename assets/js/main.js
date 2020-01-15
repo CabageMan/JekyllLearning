6d931d6d741b6836d71d7cd140df49d1929047a9
Viktor <viktor.b@ramotion.agency>
2020-01-15 11:35:00 +0300

    Experiments with JS in categories page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,26 @@ function resizeContent() {
     console.log(`The page is resizing ${ screen.width }`)
     console.log(`The window inner width ${ window.innerWidth }`)
 }
+
+function createButton() {
+    const button = document.createElement('button');
+    button.innerHTML = 'Go to About';
+    button.onclick = onButtonClick;
+    return button;
+}
+
+function createA() {
+    const link = document.createElement('a');
+    link.title = 'Go to About';
+    link.innerHTML = 'Go to About';
+    link.href = "/about/";
+    link.classList.add('button');
+    return link;
+}
+
+function onButtonClick() {
+    window.location = /about/;
+}
+
+document.body.appendChild(createButton());
+document.body.appendChild(createA());
